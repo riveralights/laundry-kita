22b303152f50a08cefd6d74d607cb7e7338c11c5
riveralights <riveralights@gmail.com>
2021-07-18 00:49:51 +0700

    menampilkan menu edit transaksi

--- admin/edit_transaction.php
+++ admin/edit_transaction.php
@@ -10,10 +10,20 @@
       <div class="card">
         <div class="card-body">
           <div class="d-flex justify-content-between align-items-center">
-          <h4 class="mb-4">Transaksi Laundry Baru</h4>
-          <a href="transaction.php" class="btn btn-sm btn-primary"><i class="bi bi-arrow-left"></i> Kembali</a>
+            <h4 class="mb-4">Edit Transaksi Laundry</h4>
+            <a href="transaction.php" class="btn btn-sm btn-primary"><i class="bi bi-arrow-left"></i> Kembali</a>
           </div>
-          <form action="transaction_action.php" method="POST">
+
+          <?php 
+            // menangkap id yang dikirimkan melalui URL
+            $id = $_GET['id'];
+
+            // mengambil data pelanggan berdasarkan id di atas
+            $query = mysqli_query($connection, "SELECT * FROM transaksi where transaksi_id='$id'");
+            while( $transaksi = mysqli_fetch_array($query) ) {
+          ?>
+          <form action="update_transaction.php" method="POST">
+            <input type="hidden" name="id" value="<?= $transaksi['transaksi_id'] ?>">
             <div class="mb-3">
               <label for="nama" class="form-label">Pelanggan</label>
               <select name="pelanggan" id="pelanggan" class="form-select">
@@ -23,17 +33,17 @@
                     $query = mysqli_query($connection, "SELECT * FROM pelanggan");
                     while( $pelanggan = mysqli_fetch_array($query) ) {
                   ?>
-                  <option value="<?= $pelanggan['pelanggan_id'] ?>"><?= $pelanggan['pelanggan_nama'] ?></option>
+                  <option <?php if($pelanggan['pelanggan_id'] == $transaksi['transaksi_pelanggan']) { echo "selected='selected'"; } ?> value="<?= $pelanggan['pelanggan_id'] ?>"><?= $pelanggan['pelanggan_nama'] ?></option>
                   <?php } ?>
               </select>
             </div>
             <div class="mb-3">
               <label for="berat" class="form-label">Berat Cucian</label>
-              <input type="number" name="berat" id="berat" class="form-control" placeholder="Berat Cucian (Kg)" required>
+              <input type="number" name="berat" id="berat" class="form-control" value="<?= $transaksi['transaksi_berat'] ?>" placeholder="Berat Cucian (Kg)" required>
             </div>
             <div class="mb-3">
               <label for="tglSelesai" class="form-label">Tanggal Selesai</label>
-              <input type="date" name="tgl_selesai" id="tglSelesai" class="form-control" required>
+              <input type="date" name="tgl_selesai" id="tglSelesai" value="<?= $transaksi['transaksi_tgl_selesai'] ?>" class="form-control" required>
             </div>
             <div class="mb-3">
                 <table class="table table-bordered table-striped">
@@ -44,10 +54,20 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php 
+                          // menyimpan id transaksi ke variabel transaksi_id
+                          $transaksi_id = $transaksi['transaksi_id'];
+
+                          // Menampilkan pakaian dari transaksi yang berid di atas
+                          $query = mysqli_query($connection, "SELECT * FROM pakaian WHERE pakaian_transaksi='$transaksi_id'");
+
+                          while( $pakaian = mysqli_fetch_array($query) ) {
+                        ?>
                         <tr>
-                            <td><input type="text" class="form-control" name="jenis_pakaian[]" id="jenisPakaian"></td>
-                            <td><input type="number" class="form-control" name="jumlah_pakaian[]" id="jumlahPakaian"></td>
+                            <td><input type="text" class="form-control" name="jenis_pakaian[]" id="jenisPakaian" value="<?= $pakaian['pakaian_jenis'] ?>"></td>
+                            <td><input type="number" class="form-control" name="jumlah_pakaian[]" id="jumlahPakaian" value="<?= $pakaian['pakaian_jumlah'] ?>"></td>
                         </tr>
+                        <?php } ?>
                         <tr>
                             <td><input type="text" class="form-control" name="jenis_pakaian[]" id="jenisPakaian"></td>
                             <td><input type="number" class="form-control" name="jumlah_pakaian[]" id="jumlahPakaian"></td>
@@ -71,10 +91,19 @@
                     </tbody>
                 </table>
             </div>
+            <div class="alert alert-primary mb-3">
+              <label for="status" class="form-label">Status</label>
+              <select name="status" id="status" class="form-select">
+                <option <?php if( $transaksi['transaksi_status'] == "0" ){ echo "selected='selected'"; } ?>>Proses</option>
+                <option <?php if( $transaksi['transaksi_status'] == "1" ){ echo "selected='selected'"; } ?>>Dicuci</option>
+                <option <?php if( $transaksi['transaksi_status'] == "2" ){ echo "selected='selected'"; } ?>>Selesai</option>
+              </select>
+            </div>
             <div class="grid grid-gap-2">
               <button type="submit" class="btn btn-success">Simpan</button>
             </div>
           </form>
+          <?php } ?>
         </div>
       </div>
     </div>
--- admin/transaction.php
+++ admin/transaction.php
@@ -3,16 +3,16 @@
 <div class="container mt-5">
   <div class="row justify-content-center">
     <div class="col-md-12">
-    <?php 
-        if(isset($_GET['message'])) {
-          if($_GET['message'] == 'success')  {
-            echo "<div class='alert alert-success text-center'><strong>Sukses!</strong> data telah ditambahkan</div>";
-          } else if($_GET['message'] == 'updated')  {
-            echo "<div class='alert alert-success text-center'><strong>Sukses!</strong> data berhasil di perbaharui</div>";
-          } else if($_GET['message'] == 'deleted')  {
-            echo "<div class='alert alert-warning text-center'><strong>Sukses!</strong> data berhasil di hapus</div>";
-          }
+      <?php
+      if (isset($_GET['message'])) {
+        if ($_GET['message'] == 'success') {
+          echo "<div class='alert alert-success text-center'><strong>Sukses!</strong> data telah ditambahkan</div>";
+        } else if ($_GET['message'] == 'updated') {
+          echo "<div class='alert alert-success text-center'><strong>Sukses!</strong> data berhasil di perbaharui</div>";
+        } else if ($_GET['message'] == 'deleted') {
+          echo "<div class='alert alert-warning text-center'><strong>Sukses!</strong> data berhasil di hapus</div>";
         }
+      }
       ?>
       <div class="card">
         <div class="card-body">
@@ -35,40 +35,40 @@
               </tr>
             </thead>
             <tbody>
-              <?php 
-                // get connection from DB
-                include '../connection.php';
+              <?php
+              // get connection from DB
+              include '../connection.php';
 
-                // query data from pelanggan table
-                $query = mysqli_query($connection, "SELECT * FROM pelanggan, transaksi WHERE transaksi_pelanggan = pelanggan_id ORDER BY transaksi_id DESC");
-                $no = 1;
-                while($transaksi = mysqli_fetch_array($query)) { ?>       
-                  <tr>      
-                    <td class="text-center"><?= $no++ ?></td>
-                    <td class="text-center">INVOICE-<?= $transaksi['transaksi_id'] ?></td>
-                    <td class="text-center"><?= $transaksi['transaksi_tanggal'] ?></td>
-                    <td class="text-center"><?= $transaksi['pelanggan_nama'] ?></td>
-                    <td class="text-center"><?= $transaksi['transaksi_berat'] ?> Kg</td>
-                    <td class="text-center"><?= $transaksi['transaksi_tgl_selesai'] ?></td>
-                    <td class="text-center"><?= "Rp. " . number_format($transaksi['transaksi_harga']) . ",-"?></td>
-                    <td class="text-center">
-                        <?php 
-                            if($transaksi['transaksi_status'] == '0') {
-                                echo "<span class='badge bg-warning'>Proses</span>";
-                            } else if($transaksi['transaksi_status'] == '1') {
-                                echo "<span class='badge bg-primary'>Dicuci</span>";
-                            } else if($transaksi['transaksi_status'] == '0') {
-                                echo "<span class='badge bg-success'>Selesai</span>";
-                            }
-                        
-                        ?>
-                    </td>
-                    <td class="text-center">
-                      <a href="invoice_transaction.php?id=<?= $transaksi['transaksi_id'] ?>" class="btn btn-success btn-sm"><i class="bi bi-receipt"></i> Invoice</a>
-                      <a href="edit_transaction.php?id=<?= $transaksi['transaksi_id'] ?>" class="btn btn-warning btn-sm"><i class="bi bi-pencil-fill"></i> Edit</a>
-                      <a href="delete_transaction.php?id=<?= $transaksi['transaksi_id'] ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin membatalkan transaksi?');"><i class="bi bi-trash-fill"></i> Batal</a>
-                    </td>
-                  </tr> 
+              // query data from pelanggan table
+              $query = mysqli_query($connection, "SELECT * FROM pelanggan, transaksi WHERE transaksi_pelanggan = pelanggan_id ORDER BY transaksi_id DESC");
+              $no = 1;
+              while ($transaksi = mysqli_fetch_array($query)) { ?>
+                <tr>
+                  <td class="text-center"><?= $no++ ?></td>
+                  <td class="text-center">INVOICE-<?= $transaksi['transaksi_id'] ?></td>
+                  <td class="text-center"><?= $transaksi['transaksi_tanggal'] ?></td>
+                  <td class="text-center"><?= $transaksi['pelanggan_nama'] ?></td>
+                  <td class="text-center"><?= $transaksi['transaksi_berat'] ?> Kg</td>
+                  <td class="text-center"><?= $transaksi['transaksi_tgl_selesai'] ?></td>
+                  <td class="text-center"><?= "Rp. " . number_format($transaksi['transaksi_harga']) . ",-" ?></td>
+                  <td class="text-center">
+                    <?php
+                    if ($transaksi['transaksi_status'] == '0') {
+                      echo "<span class='badge bg-warning'>Proses</span>";
+                    } else if ($transaksi['transaksi_status'] == '1') {
+                      echo "<span class='badge bg-primary'>Dicuci</span>";
+                    } else if ($transaksi['transaksi_status'] == '0') {
+                      echo "<span class='badge bg-success'>Selesai</span>";
+                    }
+
+                    ?>
+                  </td>
+                  <td class="text-center">
+                    <a href="invoice_transaction.php?id=<?= $transaksi['transaksi_id'] ?>" class="btn btn-success btn-sm"><i class="bi bi-receipt"></i> Invoice</a>
+                    <a href="edit_transaction.php?id=<?= $transaksi['transaksi_id'] ?>" class="btn btn-warning btn-sm"><i class="bi bi-pencil-fill"></i> Edit</a>
+                    <a href="delete_transaction.php?id=<?= $transaksi['transaksi_id'] ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin membatalkan transaksi?');"><i class="bi bi-trash-fill"></i> Batal</a>
+                  </td>
+                </tr>
               <?php } ?>
             </tbody>
           </table>
